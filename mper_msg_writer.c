26f3626142b2e6ecdcbc3a2169a0ada36c6752be
Young Hyun <youngh@caida.org>
2009-09-03 18:45:11 -0700

    Fixed some bugs and added more checks to mper_msg_writer.

--- mper_msg_writer.c
+++ mper_msg_writer.c
@@ -37,6 +37,17 @@
 static char message_buf[MPER_MSG_MAX_MESSAGE_SIZE + 1];  /* + NUL-term */
 static char sprintf_buf[MPER_MSG_MAX_MESSAGE_SIZE + 1];  /* + NUL-term */
 
+/*
+** {name_buf} stores {words[i].cw_name} values synthesized during the execution
+** of create_control_message().  This buffer contains a sequence of (adjacent)
+** NUL-terminated strings (that is, name1\0name2\0name3\0...).
+*/
+static char name_buf[MPER_MSG_MAX_MESSAGE_SIZE + 1];  /* + NUL-term */
+
+/* ====================================================================== */
+
+static char *strcpy_p(char *s1, const char *s2);
+
 /* ====================================================================== */
 
 
@@ -53,6 +64,7 @@ const char *
 create_control_message(control_word_t *words, size_t length,
 		       size_t *msg_length_out)
 {
+  char *nbp = name_buf;
   const char *name;
   const struct keyword *keyword;
   size_t i;
@@ -68,7 +80,7 @@ create_control_message(control_word_t *words, size_t length,
     RETURN_ERROR;
   }
 
-  words[0].cw_name = keyword_code_names[KC_REQNUM];  /* for completeness */
+  words[0].cw_name = nbp;  nbp = strcpy_p(nbp, keyword_code_names[KC_REQNUM]);
   words[0].cw_type = KT_UINT;
 
   if (words[1].cw_code <= KC_CMD_MIN || words[1].cw_code >= KC_CMD_MAX) {
@@ -78,10 +90,10 @@ create_control_message(control_word_t *words, size_t length,
     RETURN_ERROR;
   }
 
-  /* somewhat convoluted way of getting the type since type isn't exported */
+  /* XXX somewhat convoluted way of getting at the unexported type */
   name = keyword_code_names[words[1].cw_code];
   keyword = in_word_set(name, strlen(name));
-  words[1].cw_name = name;
+  words[1].cw_name = nbp;  nbp = strcpy_p(nbp, name);
   words[1].cw_type = keyword->type;
 
   for (i = 2; i < length; i++) {
@@ -94,7 +106,7 @@ create_control_message(control_word_t *words, size_t length,
 
     name = keyword_code_names[words[i].cw_code];
     keyword = in_word_set(name, strlen(name));
-    words[i].cw_name = name;
+    words[i].cw_name = nbp;  nbp = strcpy_p(nbp, name);
     words[i].cw_type = keyword->type;
   }
 
@@ -151,7 +163,7 @@ marshal_control_message(const control_word_t *words, size_t length,
   }
 
   l = sprintf(sprintf_buf, "%lu %s", (unsigned long)words[0].cw_uint,
-	      words[0].cw_name);
+	      words[1].cw_name);
   APPEND_MSG(s, l);
 
   for (i = 2; i < length; i++) {
@@ -171,27 +183,36 @@ marshal_control_message(const control_word_t *words, size_t length,
 
     case KT_STR:
     case KT_BLOB:
-      if (words[i].cw_len > MPER_MSG_MAX_RAW_VALUE_SIZE) {
+      if (words[i].cw_len == 0) {
+	sprintf(message_buf,
+		"%s/%s value has cw_len == 0 in control word at index %d",
+		keyword_type_names[KT_STR], keyword_type_names[KT_BLOB],
+		(int)i);
+	RETURN_ERROR;
+      }
+      else if (words[i].cw_len > MPER_MSG_MAX_RAW_VALUE_SIZE) {
 	sprintf(message_buf, "%s/%s value is too long in control word at index %d; max length = %d, got %d",
 		keyword_type_names[KT_STR], keyword_type_names[KT_BLOB],
 		(int)i, MPER_MSG_MAX_RAW_VALUE_SIZE, (int)words[i].cw_len);
+	RETURN_ERROR;
       }
+
       src = (words[i].cw_type == KT_STR ?
 	     (const unsigned char *)words[i].cw_str : words[i].cw_blob);
-      base64_encode(src, words[i].cw_len, sprintf_buf);
-      l = (int)words[i].cw_len;
+      *sprintf_buf = '$'; /* note: add 1 to l below to adjust for this '$' */
+      l = (int)base64_encode(src, words[i].cw_len, sprintf_buf + 1) + 1;
       break;
 
     case KT_SYMBOL:
-      l = sprintf(sprintf_buf, "%s", words[i].cw_symbol);
+      l = sprintf(sprintf_buf, ":%s", words[i].cw_symbol);
       break;
 
     case KT_ADDRESS:
-      l = sprintf(sprintf_buf, "%s", words[i].cw_address);
+      l = sprintf(sprintf_buf, "@%s", words[i].cw_address);
       break;
 
     case KT_PREFIX:
-      l = sprintf(sprintf_buf, "%s", words[i].cw_prefix);
+      l = sprintf(sprintf_buf, "@%s", words[i].cw_prefix);
       break;
 
     case KT_TIMEVAL:
@@ -201,7 +222,8 @@ marshal_control_message(const control_word_t *words, size_t length,
 		(int)i);
 	RETURN_ERROR;
       }
-      l = sprintf(sprintf_buf, "T%lu:%lu", (unsigned long)words[i].cw_timeval.tv_sec,
+      l = sprintf(sprintf_buf, "T%lu:%lu",
+		  (unsigned long)words[i].cw_timeval.tv_sec,
 		  (unsigned long)words[i].cw_timeval.tv_usec);
       break;
 
@@ -216,4 +238,18 @@ marshal_control_message(const control_word_t *words, size_t length,
 
   *msg_length_out = s - message_buf;
   return message_buf;
+}
+
+
+/* ====================================================================== */
+/* Returns a pointer to just past the \0 of {s1} after copying {s2} to {s1}. */
+static char *
+strcpy_p(char *s1, const char *s2)
+{
+  while (*s2) {
+    *s1++ = *s2++;
+  }
+
+  *s1++ = '\0';
+  return s1;
 }
