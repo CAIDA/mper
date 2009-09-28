66d0908025a9d65668cc590be60e0967995982cf
Young Hyun <youngh@caida.org>
2009-09-28 14:43:09 -0700

    Added Windows typedefs.

--- systypes.h
+++ systypes.h
@@ -26,6 +26,9 @@
 typedef unsigned __int8 uint8_t;
 typedef unsigned __int16 uint16_t;
 typedef unsigned __int32 uint32_t;
+typedef int ssize_t;
+typedef int pid_t;
+typedef int socklen_t;
 #define __func__ __FUNCTION__
 #endif
 
