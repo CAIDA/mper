9d6edb9c0f49ec49024c2a5eff1a3aff60b6f511
Alistair King <alistair@caida.org>
2011-09-01 15:34:23 -0700

    make into proper shell script with useful messages
    - added sh formatting and shebang
    - altered so it halts compilation on an error
    - made the error messages more verbose so people know what is going on.

--- mper_keywords.gperf.sh
+++ mper_keywords.gperf.sh
@@ -1,12 +1,21 @@
-	if $1 $2 \
-            mper_keywords.gperf >$3~; then \
-	    mv $3~ $3; \
-            elif $1 --version >/dev/null 2>&1; then \
-	    echo "WARNING: gperf failed. $3 may not be up to date"; \
-            rm $3~; \
-            exit 0; \
-            else \
-	    echo "WARNING: gperf not installed. $3 may not be up to date"; \
-            rm $3~; \
-            touch $3; \
-            fi
+#!/bin/sh
+
+if $1 $2 mper_keywords.gperf >$3~; then
+    mv $3~ $3;
+elif $1 --version >/dev/null 2>&1; then
+    printf "\n\nERROR: gperf failed. $3 may not be up to date.\n";
+    printf "  This likely means you have a problem with mper_keywords.gperf,\n";
+    printf "  but it can mean that you are using an old version of gperf.\n";
+    printf "  If you have not modified mper_keywords.gperf, then it is probably\n";
+    printf "  safe to touch mper_keywords.c and re-run make, otherwise,\n";
+    printf "  you should ensure you have gperf 3.x or higher and that there are\n";
+    printf "  no problems in mper_keywords.gperf\n";
+    rm $3~;
+    exit 1;
+else
+    printf "\n\nERROR: gperf not installed. $3 may not be up to date.\n";
+    printf "  If you have not modified mper_keywords.gperf, then it is probably\n";
+    printf "  safe to touch mper_keywords.c and re-run make\n";
+    rm $3~;
+    exit 1;
+fi
