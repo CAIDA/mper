018bd522c5d9ffe7952a96c194d08dfedd3a728b
Alistair King <alistair@caida.org>
2011-09-27 15:41:23 -0700

    added some more verbose output to the ac_prepare script

--- ac_prepare.sh
+++ ac_prepare.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+echo "creating m4 directory";
 mkdir -p m4;
 autoreconf --install --force --verbose;
 echo "sleeping 2 secs to ensure mper_keywords.c is newer than mper_keywords.gperf";
 sleep 2;
+echo "touching mper_keywords.c"; 
 touch mper_keywords.c;
