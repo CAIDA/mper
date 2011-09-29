2f54984e8a12618d3e4f4b8d2ca0187b4ee930e5
Alistair King <alistair@caida.org>
2011-09-29 13:13:00 -0700

    error checking and full log format in the mper_package.sh script
    - ensures that the script is being called from within an mper repo
    - outputs the full git logs to the ChangeLog rather than just the first line

--- mper_package.sh
+++ mper_package.sh
@@ -1,19 +1,36 @@
 #!/bin/sh
 
-if [ $1 -eq "" ] ;
+if test -z "$1" ;
 then
    echo "usage $0 build_name";
    exit -1;
 fi
 
+if [ ! -e mper_keywords.h ];
+then
+    echo "ERROR: mper sources not found in current directory";
+    exit -1;
+fi
+
 BUILD_NAME=$1;
 REPO_DIR=`pwd`;
+
+if ! git status ;
+then
+    echo "ERROR: must be in mper git repo";
+    exit -1;
+fi
 
 echo "extracting sources from git";
 git archive --format=tar --prefix=$BUILD_NAME/ HEAD | (cd /tmp/ && tar xvpf -)
 
 echo "updating the changelog";
-git --no-pager log --format="%ai %aN %n%n%x09* %s%d%n" > /tmp/$BUILD_NAME/ChangeLog;
+# full git logs
+git --no-pager log --format="%ai %aN %n%n%x09* %s%d%n%n%b%n" > /tmp/$BUILD_NAME/ChangeLog;
+
+# one-line git logs
+#git --no-pager log --format="%ai %aN %n%n%x09* %s%d%n" > /tmp/$BUILD_NAME/ChangeLog;
+
 cd /tmp/$BUILD_NAME;
 
 echo "preparing auto tools related files";
